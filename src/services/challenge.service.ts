f95e0bff45524da3fc4d1831716be2cb35fd6376
Thomasperge <thomaskauffmant74@gmail.com>
2024-04-19 00:32:33 +0200

    feat: when crate achallenge automatic challenge joined

--- src/services/challenge.service.ts
+++ src/services/challenge.service.ts
@@ -1,6 +1,7 @@
 import { Response } from 'express';
 import { ChallengeDto } from '../types/challenge.dto';
 import challengeSchema from '../models/challenge.model'
+import joinChallengeSchema from '../models/join_challenge.model'
 import usersSchema from '../models/user.model'
 
 async function generateChallenge(req: ChallengeDto, res: Response): Promise<void> {
@@ -18,8 +19,17 @@ async function generateChallenge(req: ChallengeDto, res: Response): Promise<void
       categorie: req.categorie,
     });
 
+    const newJoinChallenge = new joinChallengeSchema({
+      challenge_id: newChallenge._id,
+      user_id: req.generate_by_user_id,
+      completed: false,
+    });
+
     await newChallenge.validate();
     await newChallenge.save();
+
+    await newJoinChallenge.validate();
+    await newJoinChallenge.save();
 
     res.status(200).send({ message: "Challenge created successfully", status: 0 });
   } catch (error: any) {
